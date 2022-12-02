775fac0798605ee9f831d7d9883cc9bcf455fced
akmal <akmal@DESKTOP-QEPVMLM>
2022-12-02 14:40:16 +0700

    Commit

---
+++ src/main/java/com/pws/a/ProductServiceRESTfulAPI/ProductServiceResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.pws.a.ProductServiceRESTfulAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceResTfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pws/a/ProductServiceRESTfulAPI/ProductServiceResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pws.a.ProductServiceRESTfulAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
